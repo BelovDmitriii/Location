bf2892bdd47a2bea4cd4c472b7b11854eaa4d647
BelovDmitriii <belovd90@mail.ru>
2021-04-22 17:53:40 +0300

    add_distance

--- myLoc.js
+++ myLoc.js
@@ -1,5 +1,10 @@
 window.onload = getMyLocation;
 
+let ourCoords = {
+    latitude: 59.877068, 
+    longitude: 30.345234
+};
+
 function getMyLocation () {
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(displayLocation);
@@ -14,6 +19,13 @@ function displayLocation (position){
 
     let div = document.getElementById("location");
     div.innerHTML = "Ваше местоположение: широта: " + latitude + ", долгота: " + longitude;
+
+
+    // Координаты точки А и В
+
+    let km = computeDistance(position.coords, ourCoords);
+    let distance = document.getElementById("distance");
+    distance.innerHTML = "На данный момент вы на " + km + " км от Тусяныча";
 }
 
 function displayError(error) {
@@ -31,5 +43,23 @@ function displayError(error) {
 	div.innerHTML = errorMessage;
 }
 
+// distance between two coord
 
+function computeDistance(startCoords, destCoords) {
+    let startLatRads = degreesToRadians (startCoords.latitude);
+    let startLongRads = degreesToRadians (startCoords.longitude);
+    let destLatRads = degreesToRadians (destCoords.latitude);
+    let destLongRads = degreesToRadians (destCoords.longitude);
+
+    let Radius = 6371; // радиус Земли в км
+    let distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+                    Math.cos(startLatRads) * Math.cos(destLatRads) * 
+                    Math.cos(startLongRads - destLongRads)) * Radius;
+    return distance;
+}
+
+function degreesToRadians (degrees) {
+    let radians = (degrees * Math.PI) / 180;
+    return radians;
+}
 
