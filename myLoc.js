16a3ca32960213859c5f91867499cb91e047f170
BelovDmitriii <belovd90@mail.ru>
2021-04-30 11:16:11 +0300

    add_Markers_of_your_way

--- myLoc.js
+++ myLoc.js
@@ -1,9 +1,11 @@
+let options = { enableHightAccuracy: true, timeout: 100, maximumAge:0};
 window.onload = getMyLocation;
 let watchId = null;
 let ourCoords = {
     latitude: 59.877068, 
     longitude: 30.345234
 };
+let map;
 
 function getMyLocation () {
     if (navigator.geolocation){
@@ -17,17 +19,7 @@ function getMyLocation () {
     }
 }
 
-function watchLocation() {
-    watchId = navigator.geolocation.watchPosition(displayLocation, displayError);
-}
-
-function clearWatch() {
-    if(watchId) {
-        navigator.geolocation.clearWatch(watchId);
-    watchId = null;
-    alert ("Слежение за вами приостановлено");
-    }
-}
+let prevCoords = null;
 
 function displayLocation (position){
     let latitude = position.coords.latitude;
@@ -35,10 +27,8 @@ function displayLocation (position){
 
     let div = document.getElementById("location");
     div.innerHTML = "Ваше местоположение: широта: " + latitude + ", долгота: " + longitude;
-    div.innerHTML += ( " с " + position.coords.accuracy + " метров точности");
-
-
-    // Координаты точки А и В
+    div.innerHTML +=  "( с " + position.coords.accuracy + " метрами точности)";
+    div.innerHTML += " (найдено за  " + options.timeout + " миллисекунд)";
 
     let km = computeDistance(position.coords, ourCoords);
     
@@ -47,22 +37,14 @@ function displayLocation (position){
 
     if (map == null){
         showMap(position.coords);
-    }
-}
-
-function displayError(error) {
-	var errorTypes = {
-		0: "Unknown error",
-		1: "Permission denied",
-		2: "Position is not available",
-		3: "Request timeout"
-	};
-	var errorMessage = errorTypes[error.code];
-	if (error.code == 0 || error.code == 2) {
-		errorMessage = errorMessage + " " + error.message;
+		prevCoords = position.coords;
+    } else {
+		let meters = computeDistance(position.coords, prevCoords) * 1000;
+		if (meters > 20) {
+			scrollMapToPosition(position.coords);
+			prevCoords = position.coords;
+		}
 	}
-	var div = document.getElementById("location");
-	div.innerHTML = errorMessage;
 }
 
 // distance between two coord
@@ -85,26 +67,27 @@ function degreesToRadians (degrees) {
     return radians;
 }
 
-let map;
+// Show Map
 
 function showMap (coords) {
     let googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
 
     // GoogleMaps params
 
-let mapOptions = {
-    zoom: 10,     // Значение может быть от 0 до 21
+    let mapOptions = {
+    zoom: 15,     // Значение может быть от 0 до 21
     center: googleLatAndLong,
     mapTypeId: google.maps.MapTypeId.navigator   // or SATELLITE or HYBRID
-};
+    };
 
-let mapDiv = document.getElementById("map");
-map = new google.maps.Map(mapDiv, mapOptions);
+    let mapDiv = document.getElementById("map");
+    map = new google.maps.Map(mapDiv, mapOptions);
 
-let title = "Ваше местоположение";
-let content = "Вы здесь: " + coords.latitude + ", " + coords.longitude;
-addMarker(map, googleLatAndLong, title, content); 
+    let title = "Ваше местоположение";
+    let content = "Вы здесь: " + coords.latitude + ", " + coords.longitude;
+    addMarker(map, googleLatAndLong, title, content); 
 }
+
 
 // Добавление маркера местоположения на карту
 
@@ -128,3 +111,53 @@ function addMarker(map, latlong, title, content) {
     });
 }
 
+// Вывод ошибок на экран
+
+function displayError(error) {
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied",
+		2: "Position is not available",
+		3: "Превышено время ожидания"
+	};
+        var errorMessage = errorTypes[error.code];
+        if (error.code == 0 || error.code == 2) {
+		    errorMessage = errorMessage + " " + error.message;
+	    }
+        var div = document.getElementById("location");
+        div.innerHTML = errorMessage;
+        options.timeout += 100;
+        navigator.geolocation.getCurrentPosition(
+        displayLocation,
+        displayError,
+        options);
+    div.innerHTML += " ... проверяем снова с задержкой " + options.timeout; 
+}
+
+function watchLocation() {
+    watchId = navigator.geolocation.watchPosition(
+                        displayLocation, 
+                        displayError,
+                        options);
+}
+
+//show new position
+
+function scrollMapToPosition(coords){
+    let latitude = coords.longitude;
+    let longitude = coords.longitude;
+    let latlong = new google.maps.LatLng(latitude, longitude);
+
+    map.panTo(latlong);
+
+    addMarker(map, latlong, "Ваше новое местоположение", "Вы продвинулись на позицию: " + 
+                                latitude + ", " + longitude);
+}
+
+function clearWatch() {
+    if(watchId) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+        alert ("Слежение за вами приостановлено");
+    }
+}
